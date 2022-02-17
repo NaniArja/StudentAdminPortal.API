faac889ce2b385cb07cff6163185408a8aac802e
NaniArja <nani.arja18@gmail.com>
2022-02-17 06:10:44 +0530

    End of section-2 API

---
+++ StudentAdminPortal.API/DataModels/Address.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StudentAdminPortal.API.DataModels
+{
+    public class Address
+    {
+        public Guid id { get; set; }
+        public string PhysicalAddress { get; set; }
+        public string PostalAddress { get; set; }
+
+        //Navigation property
+        public Guid StudentId { get; set; }
+    }
+}
